a1eac9197944c0dacd4458aad79a9b711c612152
DriAzz <driazz106@gmail.com>
2019-09-06 06:51:53 -0600

    test

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
-import Project from "./Project";
+import React, {Component} from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Project />
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,9 +1,35 @@
 var express = require("express");
 var router = express.Router();
+const mysql = require('mysql');
+const port = process.env.PORT || 5000;
+const app = express();
+
+//Console.log that server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "Password1!",
+  database: "products"
+});
+
+connection.connect(function(err) {
+  if(err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Database Connected!!");
+});
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+//GET response for backend
+app.get('/express_backend', (req, res, next) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
 module.exports = router;
